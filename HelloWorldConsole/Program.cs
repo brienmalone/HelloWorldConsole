ff50294c696bdfdd7fed89b5f389e5ecc7dd5199
Brien Malone <brienmalone@yahoo.com>
2017-05-09 16:25:03 -0700

    C_Sharp_Basics_Section_1 Hello World

--- HelloWorldConsole/Program.cs
+++ HelloWorldConsole/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace HelloWorldConsole
 {
@@ -10,6 +7,8 @@ namespace HelloWorldConsole
     {
         static void Main(string[] args)
         {
+           Console.WriteLine("Hello World");
+           Debug.WriteLine("Debug Mode is On!");
         }
     }
 }
